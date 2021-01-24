447426b37cb3747e2df9145d4179d7a9ef722471
Moyise <60372632+Moyise@users.noreply.github.com>
2021-01-24 02:11:34 -0500

    5th commit

--- backend/server.js
+++ backend/server.js
@@ -33,11 +33,11 @@ app.use("/api/upload", uploadRoutes);
 app.get("/api/config/paypal", (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  const root = path.join("frontend", "build");
+  console.log(root);
+  app.use(express.static(root));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve("frontend", "build", "index.html"))
-  );
+  app.get("*", (req, res) => res.sendFile("index.html", { root }));
 } else {
   app.get("/", (req, res) => res.send("API is running..."));
 }
